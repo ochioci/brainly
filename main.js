24695aafc12d8f2f5d93f39bd3564bd0f4112ae1
Beckett Randlett <brandlett1549@bths.edu>
2021-05-30 16:31:04 -0400

    init

---
+++ main.js
@@ -0,0 +1,14 @@
+const { BrainlyAPI, Server } = require('brainly-api');
+
+BrainlyAPI.startWorker({ experimental: true, server: Server.PH }, async brainly => {
+  // find question
+    test(await brainly.findQuestion('An online company lets you download songs for $0.99 each after you have paid a $5 membership fee. Which domain would be most appropriate to calculate the cost to download songs?'))
+});
+
+function test (questions) {
+    console.log(Object.keys(questions))
+    let questionDetails = questions._questionDetails
+    for (let i = 0; i < questionDetails.length; i++) {
+        console.log(questionDetails[i])
+    }
+}
